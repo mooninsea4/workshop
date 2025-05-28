06d72fa0fa6d2a30ee2c868264621a564884c9b6
mooninsea <jellyfishindeep@gmail.com>
2025-05-28 11:36:42 +0700

    WorkShopTest

---
+++ ChangeScene.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class ChangeScene : MonoBehaviour
+{
+    
+
+    public void Gameplay()
+    {
+        SceneManager.LoadScene("SampleScene");
+        //เปลี่ยนซีนไปเกมเพลย์
+    }
+
+
+
+}
